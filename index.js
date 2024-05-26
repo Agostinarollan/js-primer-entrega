b5bdf69f31b2fb5f19cd62100e93d7c97446a345
Agostinarollan <agostinarollan@gmail.com>
2024-05-26 13:07:33 -0300

    se finalizo el armado de la calculadora de fertilizante por hectarea

--- index.js
+++ index.js
@@ -4,40 +4,44 @@ const tb=120; // tasa de apliacion para FB
 const tc=90; // tasa de apliacion para FC
 
 let usuario= prompt("ingrese usuario");
-console.log("Bienvenido a la empresa", usuario);
+alert("Bienvenido a la empresa  "+ usuario);
+
+let carga;
+do {
+    let tipoFertilizante = prompt("Ingrese tipo de fertilizante:\n1. FA\n2. FB\n3. FC\n4. SALIR");
+    if (tipoFertilizante === "4") {
+        break; // Salir del bucle si el usuario elige salir
+    }
+    let cantidadHectareas = parseInt(prompt("Ingrese cantidad de hectáreas a trabajar:"));
+    carga = tipoFertilizante; // Asignamos el valor del tipo de fertilizante a la variable carga
+    calcularCantidadFertilizante(cantidadHectareas, tipoFertilizante);
+} while (carga !== "4");
 
 
-let tipoFertilizante= prompt("Ingrese tipo de fertilizante:\n1. FA\n2. FB\n3. FC");
+/*let tipoFertilizante= prompt("Ingrese tipo de fertilizante:\n1. FA\n2. FB\n3. FC");
 let cantidadHectareas=parseInt(prompt("ingrese cantidad de hectareas a trabajar"));
-/*if (isNaN (cantidadHectareas)){
-    console.log("La cantidad de hectáreas ingresada no es válida.");
-    cantidadHectareas=parseInt(prompt("ingrese cantidad de hectareas a trabajar"));
-    calcularCantidadFertilizante(cantidadHectareas,tipoFertilizante);
 
-} else{
-    calcularCantidadFertilizante(cantidadHectareas,tipoFertilizante);
-
-}*/
-calcularCantidadFertilizante(cantidadHectareas,tipoFertilizante);
-
-
+calcularCantidadFertilizante(cantidadHectareas,tipoFertilizante);*/
 // Función para calcular la cantidad total de fertilizante
 function calcularCantidadFertilizante (cantidadHectareas,tipoFertilizante){
     let cantidadTotalFertilizante;
 
     switch(tipoFertilizante){
+        case "1":
         case "FA":
             cantidadTotalFertilizante= ta*cantidadHectareas;
         break
+        case "2":
         case "FB":
             cantidadTotalFertilizante= tb*cantidadHectareas;
         break
+        case "3":
         case "FC":
             cantidadTotalFertilizante= tc*cantidadHectareas;
         break
         default:
-            console.log ("El tipo de fertilizante no es correcto");
+            alert ("El tipo de fertilizante no es correcto");
         break
     }
-    console.log("La cantidad total de fertilizante a utilizar es:", cantidadTotalFertilizante, " kg/hectárea");
+    alert("La cantidad total de fertilizante a utilizar es: "+cantidadTotalFertilizante+" kg/hectárea");
 }
