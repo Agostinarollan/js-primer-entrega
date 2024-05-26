391ee514e2f69278a87cac80f8a9a1639c074d1e
Agostinarollan <agostinarollan@gmail.com>
2024-05-26 11:49:13 -0300

    prueba de commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//definicion de las tasas de aplicacion de cada fetilizante (en kgxhectarea)//
+const ta=100; // tasa de apliacion para FA
+const tb=120; // tasa de apliacion para FB
+const tc=90; // tasa de apliacion para FC
+
+let usuario= prompt("ingrese usuario");
+console.log("Bienvenido a la empresa", usuario);
+
+
+let tipoFertilizante= prompt("ingrese tipo de fertilizante FA, FB, FC");
+let cantidadHectareas=parseInt(prompt("ingrese cantidad de hectareas a trabajar"));
+/*if (isNaN (cantidadHectareas)){
+    console.log("La cantidad de hectáreas ingresada no es válida.");
+    cantidadHectareas=parseInt(prompt("ingrese cantidad de hectareas a trabajar"));
+    calcularCantidadFertilizante(cantidadHectareas,tipoFertilizante);
+
+} else{
+    calcularCantidadFertilizante(cantidadHectareas,tipoFertilizante);
+
+}*/
+calcularCantidadFertilizante(cantidadHectareas,tipoFertilizante);
+
+
+// Función para calcular la cantidad total de fertilizante
+function calcularCantidadFertilizante (cantidadHectareas,tipoFertilizante){
+    let cantidadTotalFertilizante;
+
+    switch(tipoFertilizante){
+        case "FA":
+            cantidadTotalFertilizante= ta*cantidadHectareas;
+        break
+        case "FB":
+            cantidadTotalFertilizante= tb*cantidadHectareas;
+        break
+        case "FC":
+            cantidadTotalFertilizante= tc*cantidadHectareas;
+        break
+        default:
+            console.log ("El tipo de fertilizante no es correcto");
+        break
+    }
+    console.log("La cantidad total de fertilizante a utilizar es:", cantidadTotalFertilizante, " kg/hectárea");
+}
